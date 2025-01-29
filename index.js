43798bf94c1bb716959e6b16f745c61c68a194c3
KerissaGit <kerissa525@gmail.com>
2025-01-29 16:25:45 -0500

    9 Passing

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ saturdayFun('I want to bathe my dog');
 function mondayWork(activity = 'go to the office'){
     return `This Monday, I will ${activity}.`;
 }
+
+function wrapAdjective(wraps){
+    return function(activity){
+        return `You are ${wraps}${activity}${wraps}!`
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("*")
+
